192e2cdcdab495699a1adc79d3382b8bb54a0d3b
pearlbytes <itsjustpearl04@gmail.com>
2026-02-22 03:28:36 +0200

    Add solution for Finger Exercise 2: print positive, negative or zero based on input

---
+++ exercises/week2/finger_exercise_lecture2.py
@@ -0,0 +1,8 @@
+#Assume you are a given a variable named number(has a numerical value)
+number = 0 # test value
+if number > 0: 
+    print("positive") # prints 'positive' if the variable is positive
+elif number < 0:
+    print("negative") # prints 'negative' if the variable is negative
+else:
+    print("zero") # prints 'zero' if the variable number is equal to zero
