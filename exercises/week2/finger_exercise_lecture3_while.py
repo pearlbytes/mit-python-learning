5d2aead64247c2d6a1a145061a45cdc75c456e06
pearlbytes <itsjustpearl04@gmail.com>
2026-02-22 03:34:38 +0200

    Add alternative solution for Finger Exercise 3 using while loop: print 'hello world' N times with test value N=5

---
+++ exercises/week2/finger_exercise_lecture3_while.py
@@ -0,0 +1,7 @@
+# Assume you are given a positive integer variable named N.
+N = 5 # test value
+count = 0
+while count < N : # prints 'hello world' on seperate lines N times
+    print("hello world")
+    count+=1
+    
